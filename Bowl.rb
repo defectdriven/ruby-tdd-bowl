c6f832ee142a7528b8d7bf3f4f363fe1f7f3a561
Mike Ask <Mike.Ask@amnhealthcare.com>
2022-09-13 16:10:45 -0500

    passing with a previous frame spare

--- Bowl.rb
+++ Bowl.rb
@@ -3,8 +3,22 @@ class Bowler
 
   def initialize()
     @score = 0
+    @frameScore = 0
+    @ball = 1
   end
   def roll(pins)
-    @score = @score + pins
+    if (@frameScore == (10))
+      @score = @score + pins
+    end
+    case @ball
+      when 1
+        @frameScore = pins
+        @ball = 2
+      when 2
+        @score = @score + @frameScore + pins
+        puts(@score)
+        @frameScore = @frameScore + pins
+        @ball = 1
+    end
   end
 end
--- spec/bowl_spec.rb
+++ spec/bowl_spec.rb
@@ -2,30 +2,24 @@ require 'rspec'
 require_relative '..\Bowl'
 
 describe 'bowling tests' do
-  describe 'rolling a gutter game scores 0' do
     it 'when rolling a gutter game, the score will be 0' do
       bowler = Bowler.new
       bowler.roll(0)
       expect(bowler.score).to eq (0)
     end
-  end
-  describe 'rolling a 2 and a 4 results in a score of 6' do
     it 'when rolling a 2 and a 4, the score will be 6' do
       bowler = Bowler.new
       bowler.roll(2)
       bowler.roll(4)
       expect(bowler.score).to eq (6)
     end
-  end
-  describe 'rolling a spare followed by a 7 and a 2 will show score of 28' do
     it 'when rolling a spare followed by 7 and 2, the score will be 28' do
       bowler = Bowler.new
       bowler.roll(7)
       bowler.roll(3)
       bowler.roll(7)
       bowler.roll(2)
-      expect(bowler.score).to eq (28)
+      expect(bowler.score).to eq (26)
     end
-  end
 end
 
